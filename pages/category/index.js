29110dabbeca392f5319ddcbe696504b045cc85c
Alphatrionty <1379844203@qq.com>
2020-05-05 20:12:28 +0800

    分类页面

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,3 +1,4 @@
+import { request } from "../../request/index.js";
 // pages/category/index.js
 Page({
 
@@ -5,14 +6,39 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // 左侧菜单数据
+    leftMenuList:[],
+    // 右侧商品数据
+    rightContent:[],
+    // 被点击的左侧菜单
+    currentIndex:0
   },
-
+  // 接口的返回数据
+  Cates:[],
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.getCates();
+  },
 
+  // 获取分类数据
+  getCates() {
+    request({
+      url: "https://api-hmugo-web.itheima.net/api/public/v1/categories"
+    }).then(res => {
+      this.Cates=res.data.message;
+
+      // 构造左侧菜单数据
+      let leftMenuList=this.Cates.map(v=>v.cat_name);
+      // 构造右侧的商品数据
+      let rightContent=this.Cates[0].children;
+
+      this.setData({
+        leftMenuList,
+        rightContent
+      })
+    })
   },
 
   /**
